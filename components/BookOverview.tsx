78ced8cf925c569f5646039674f7abd019520a81
Adrian <contact@jsmastery.pro>
2025-01-09 19:02:27 +0100

    fix if check

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -28,10 +28,8 @@ const BookOverview = async ({
     .where(eq(users.id, userId))
     .limit(1);
 
-  if (!user) return null;
-
   const borrowingEligibility = {
-    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    isEligible: availableCopies > 0 && user?.status === "APPROVED",
     message:
       availableCopies <= 0
         ? "Book is not available"
@@ -70,11 +68,13 @@ const BookOverview = async ({
 
         <p className="book-description">{description}</p>
 
-        <BorrowBook
-          bookId={id}
-          userId={userId}
-          borrowingEligibility={borrowingEligibility}
-        />
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
 
       <div className="relative flex flex-1 justify-center">
